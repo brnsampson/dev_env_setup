8f18e67b643a4654a1f439b69def4f006e3e9b8d
Brian Sampson <bsampson@infra01.whobe.us>
2020-06-23 15:36:23 -0700

    initial commit with virtualbox, vagrant, and packer

---
+++ vbox_install.sh
@@ -0,0 +1,20 @@
+echo 'deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib' >> /etc/apt/sources.list
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O oracle_vbox_2016.asc
+if apt-key add oracle_vbox_2016.asc; then
+	echo "virtualbox key added to apt"
+else
+	echo "failed to add virtualbox key to apt"
+	exit 1
+fi
+
+apt-get update
+if apt-get install virtualbox-6.1; then
+	echo "Successfully installed virtualbox"
+else
+	echo "Failed to install virtualbox"
+	exit 2
+fi
+rm oracle_vbox_2016.asc
+
+echo ""
